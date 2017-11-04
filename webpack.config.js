e7e035f1f4c32fae2a5210b64d2b651d8618937f
Justin Kennedy <jekennedy@umass.edu>
2017-11-03 20:14:22 -0400

    webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-
+  entry: './app/index.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
 }
