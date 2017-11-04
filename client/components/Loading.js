d1c99bd85cddca7478e1c9b1fe5cd2d3d7a6bccc
Matt Gramigna <mgramigna@umass.edu>
2017-11-04 12:52:39 -0400

    Fixed className in Loading

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loading extends Component {
     render() {
         return (
-          <div class="loading text-center">
+          <div className="loading text-center">
             <img src="https://cdn-us-east.velaro.com/Content/Images/loading.gif"/>
           </div>
         );
