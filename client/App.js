e7624c3ab3b359a0b5412b9490841d4d9c3be420
Matt Gramigna <mgramigna@umass.edu>
2017-11-03 23:22:45 -0400

    Init MUI dark theme

--- client/App.js
+++ client/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <h1>Hello world</h1>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <h1>SpotiFind</h1>
       </MuiThemeProvider>
     );
   }
