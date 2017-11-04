78b70b4e64c3bfa0363ecd4b6f774c047555635a
Matt Gramigna <mgramigna@umass.edu>
2017-11-04 07:15:55 -0400

    moved auth to own module

--- server/authentication.js
+++ server/authentication.js
@@ -1,10 +1,57 @@
+const db = require('./db/controller');
+const config = require('./config.json');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const SpotifyStrategy = require('passport-spotify').Strategy;
 
-var authenticate = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    res.redirect('/auth/');
-  } else {
-    return next();
-  }
+module.exports = function(app, passport) {
+  app.use(session({
+    secret: config.sessionSecret,
+    saveUninitialized: true,
+    resave: true,
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new SpotifyStrategy({
+      clientID: config.spotifyClientId,
+      clientSecret: config.spotifySecret,
+      callbackURL: "/auth/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      db.saveUser({
+        id: profile.id,
+        access_token: accessToken,
+        refresh_token: refreshToken
+      })
+      return done(null, profile);
+    }
+  ));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
+  app.get('/', function(req, res) {
+    if (req.isAuthenticated()) {
+      res.redirect('/home');
+    } else {
+      res.sendFile(path.resolve(__dirname, '..', 'build/landing.html'));
+    }
+  });
+
+  app.get('/auth/',
+    passport.authenticate('spotify', {scope: ['user-read-email', 'user-read-private'], showDialog: true}),
+    function(req, res) {}
+  );
+
+  app.get('/auth/callback',
+    passport.authenticate('spotify', { failureRedirect: '/' }),
+    function(req, res) {
+      res.redirect('/home');
+  });
 }
-
-module.exports = { authenticate }
--- server/server.js
+++ server/server.js
@@ -2,63 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const passport = require('passport');
-const session = require('express-session');
-const SpotifyStrategy = require('passport-spotify').Strategy;
 
-const db = require('./db/controller');
-const config = require('./config.json');
-const authenticate = require('./authentication').authenticate;
-
-app.use(session({
-  secret: config.sessionSecret,
-  saveUninitialized: true,
-  resave: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new SpotifyStrategy({
-    clientID: config.spotifyClientId,
-    clientSecret: config.spotifySecret,
-    callbackURL: "/auth/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    db.saveUser({
-      id: profile.id,
-      access_token: accessToken,
-      refresh_token: refreshToken
-    })
-    return done(null, profile);
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-app.get('/', function(req, res) {
-  if (req.isAuthenticated()) {
-    res.redirect('/home');
+function authenticate(req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.redirect('/auth/');
   } else {
-    res.sendFile(path.resolve(__dirname, '..', 'build/landing.html'));
+    return next();
   }
-});
+}
+
+require('./authentication')(app, passport);
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
-
-app.get('/auth/',
-  passport.authenticate('spotify', {scope: ['user-read-email', 'user-read-private'], showDialog: true}),
-  function(req, res) {}
-);
-
-app.get('/auth/callback',
-  passport.authenticate('spotify', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/home');
-});
 
 require('./api')(app, authenticate);
 
