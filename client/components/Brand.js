b36e24b8679a7cc8099e80aedc208e2e95806563
Matt Gramigna <mgramigna@umass.edu>
2017-11-04 12:51:44 -0400

    Made logo a link to homepage

--- client/components/Brand.js
+++ client/components/Brand.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Brand extends Component {
     render() {
         return (
           <div className="media brand">
-            <div className="media-left">
-              <img className="media-object" src="https://seeklogo.com/images/S/spotify-2015-logo-560E071CB7-seeklogo.com.png"/>
-            </div>
-            <div className="media-body media-middle">
-              <div>Spotifind</div>
-            </div>
+            <Link to="/home">
+              <div className="media-left">
+                <img className="media-object" src="https://seeklogo.com/images/S/spotify-2015-logo-560E071CB7-seeklogo.com.png"/>
+              </div>
+              <div className="media-body media-middle">
+                <div>Spotifind</div>
+              </div>
+            </Link>
           </div>
         )
     }
