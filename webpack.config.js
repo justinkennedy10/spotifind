c1903723d013ee124bf9175dff65565fd6d8b402
Matt Gramigna <mgramigna@umass.edu>
2017-11-03 20:29:04 -0400

    Added initial react directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
-  entry: './app/index.js',
+  entry: './client/index.js',
   output: {
-    path: './build',
+    path: path.join(__dirname, '/build'),
     filename: 'bundle.js',
     publicPath: '/'
   },
