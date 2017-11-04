98c946ff2e1a983e698f99da60c9826b4728ba6a
Andrew Bass <arbass@umass.edu>
2017-11-04 06:17:00 -0400

    Hooked playlist endpoint to list views

--- client/components/PlaylistList.js
+++ client/components/PlaylistList.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PlaylistItem from './PlaylistItem';
 
 class PlaylistList extends Component {
-    render() {
-        var playlists = [{
-          "id": 1,
-          "name": "Playlist1",
-          "members": "Jonh, Justn",
-          "dateCreated": "10-24-16",
-        },
-        {
-          "id": 2,
-          "name": "Playlist2",
-          "members": "Jonh, Mike",
-          "dateCreated": "9-2-17",
-        }];
-        return (
-          <div className="playlist-list">
-          {playlists.map((playlist) => (
-              <PlaylistItem key={playlist.id} playlist={playlist}/>
-            ))}
-          </div>
-        );
+  constructor(props) {
+    super(props)
+    this.state = {
+      playlists: []
     }
+  }
+
+  componentDidMount() {
+    let ref = this;
+    axios.get('/api/mgramigna4/playlists').then(function(res) {
+      ref.setState({playlists: res.data});
+    });
+  }
+
+  render() {
+      return (
+        <div className="playlist-list">
+        {this.state.playlists.map((playlist) => (
+            <PlaylistItem key={playlist.id} playlist={playlist}/>
+          ))}
+        </div>
+      );
+  }
 }
 
 export default PlaylistList;
