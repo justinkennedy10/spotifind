b44c705e504e078852e1d2dcdf09db11489048c3
Matt Gramigna <mgramigna@umass.edu>
2017-11-05 03:43:03 -0500

    All set to process algorithm

--- server/generator.js
+++ server/generator.js
@@ -1,11 +1,6 @@
 const spotify = require('./spotify');
 const User = require('./User');
-
-const sizes = {
-  's': 25,
-  'm': 50,
-  'l': 100
-}
+const dedupe = require('dedupe');
 
 module.exports = {
   generatePlaylist(playlist) {
@@ -16,8 +11,8 @@ module.exports = {
       getRecommendations(seeds, targets)
         .then(track_list => {
           track_list = orderByPopularity(track_list);
-          track_list = selectTracksBySize(tracks, playlist.size)
-          spotify.uploadPlaylist(user, playlist.name, tracks)
+          track_list = selectTracksBySize(track_list, playlist.size)
+          spotify.uploadPlaylist(user, playlist.name, track_list)
             .then(spotify_id => resolve(spotify_id))
             .catch((error, statusCode) => reject(statusCode));
         })
@@ -27,11 +22,61 @@ module.exports = {
 }
 
 function getSeeds(users) {
-
+  const SEED_COUNT = 10;
+  let tracks = [], artists = [], top_tracks = [];
+  users.forEach(function (user) {
+    tracks = tracks.concat(user.recently_played);
+    top_tracks = top_tracks.concat(user.top_tracks.slice(0,SEED_COUNT));
+    artists = artists.concat(user.top_artists);
+  });
+  tracks = orderByPopularity(tracks);
+  artists = orderByPopularity(artists);
+  return {
+    tracks: tracks.concat(top_tracks),
+    artists: artists,
+    genres: []
+  }
 }
 
 function getTargets(type) {
+  const TYPE_VALUES = {
+    'party': {
+      danceability: 1.0,
+      energy: 1.0,
+      valence: 0.8
+    },
+    'road trip': {
+      acousticness: 0.8,
+      danceability: 0.5,
+      energy: 0.6,
+      valence: 0.8
+    },
+    'chill_hang': {
+      acousticness: 1.0,
+      danceability: 0.1,
+      instrumentalness: 0.6
+    },
+    'romantic': {
+      instrumentalness: 1.0,
+      acousticness: 0.8,
+      danceability: 0.2
+    },
+    'focus': {
+      instrumentalness: 1.0,
+      acousticness: 1.0,
+      energy: 0.1
+    }
+  }
 
+  let targets = [];
+  for(var attr in TYPE_VALUES[type]) {
+    targets.push({
+      attribute: attr,
+      value: TYPE_VALUES[type][attr]
+    })
+  }
+
+  return targets;
 }
 
 function getRecommendations(seeds, targets) {
@@ -41,9 +86,23 @@ function getRecommendations(seeds, targets) {
 }
 
 function orderByPopularity(track_list) {
+  let counts = {};
 
+  track_list.forEach(function (t) {
+    counts[t.id] = (counts[t.id] || 0) + 1
+  });
+
+  return dedupe(track_list).sort(function (t1, t2) {
+    return counts[t1.id] < counts[t2.id];
+  });
 }
 
 function selectTracksBySize(track_list, size) {
+  const sizes = {
+    's': 25,
+    'm': 50,
+    'l': 100
+  }
 
+  return track_list.slice(0, sizes[size]);
 }
