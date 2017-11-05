914ccd3e6c028b22b256e8f46ec1bdf487c02be1
Justin Kennedy <jekennedy@umass.edu>
2017-11-05 02:06:49 -0500

    spacing

--- server/generator.js
+++ server/generator.js
@@ -2,26 +2,26 @@ const spotify = require('./spotify');
 const User = require('./User');
 
 const sizes = {
-	's': 25,
-	'm': 50,
-	'l': 100
+  's': 25,
+  'm': 50,
+  'l': 100
 }
 
 module.exports = {
   generatePlaylist(playlist) {
     return new Promise((resolve, reject) => {
-			var seeds = getSeeds(playlist.users);
-			var targets = getTargets(playlist.type);
+      var seeds = getSeeds(playlist.users);
+      var targets = getTargets(playlist.type);
 
-			getRecommendations(seeds, targets)
-				.then(track_list => {
-					track_list = orderByPopularity(track_list);
-					track_list = selectTracksBySize(tracks, playlist.size)
-					spotify.uploadPlaylist(user, playlist.name, tracks)
-						.then(spotify_id => resolve(spotify_id))
-						.catch((error, statusCode) => reject(statusCode));
-				})
-				.catch(err => { reject(err)});
+      getRecommendations(seeds, targets)
+        .then(track_list => {
+          track_list = orderByPopularity(track_list);
+          track_list = selectTracksBySize(tracks, playlist.size)
+          spotify.uploadPlaylist(user, playlist.name, tracks)
+            .then(spotify_id => resolve(spotify_id))
+            .catch((error, statusCode) => reject(statusCode));
+          })
+          .catch(err => reject(err));
     });
   }
 }
@@ -35,9 +35,9 @@ function getTargets(type) {
 }
 
 function getRecommendations(seeds, targets) {
-	return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
 			
-	});
+  });
 }
 
 function orderByPopularity(track_list) {
