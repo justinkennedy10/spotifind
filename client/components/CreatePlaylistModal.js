2b5322cb029cbd5d072c4c24f362aad81d5b23db
Andrew Bass <arbass@umass.edu>
2017-11-05 06:53:37 -0500

    Styled modal

--- client/components/CreatePlaylistModal.js
+++ client/components/CreatePlaylistModal.js
@@ -6,10 +6,16 @@ class CreatePlaylistModal extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {pid: uuidv4(), history: props.history};
+    this.state = {
+      pid: uuidv4(),
+      history: props.history,
+      type: "Party",
+      size: "Small"
+    };
   }
 
   selectOption(e) {
+    console.log(e);
     if(e.target.name === 'typeOptions') {
       this.setState({
         type: e.target.value
@@ -58,48 +64,60 @@ class CreatePlaylistModal extends Component {
       }];
 
         return (
-          <div id="createPlaylistModal" className="modal fade new-playlist" role="dialog">
+           <div id="createPlaylistModal" className="modal fade new-playlist" role="dialog">
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
                   <button type="button" className="close" data-dismiss="modal">&times;</button>
                 </div>
-                <div className="modal-body">
-                  <form>
-                    <div className="form-group">
-                      <input className="text name" className="form-control" placeholder="Playlist Name" onChange={this.selectOption.bind(this)}/>
-                    </div>
-                    <div className="form-group text-center">
-                      {typeOptions.map(opt => (
-                        <label key={opt.value} className="radio-inline">
-                          <input type="radio" name="typeOptions" value={opt.value} onChange={this.selectOption.bind(this)} /> {opt.label}
-                        </label>
-                      ))}
-                    </div>
-                    <div className="form-group text-center">
-                      <label className="radio-inline">
-                        <input type="radio" name="sizeOptions" value="s" onChange={this.selectOption.bind(this)} /> Small (20-25 Songs)
-                      </label>
-                      <label className="radio-inline">
-                        <input type="radio" name="sizeOptions" value="m" onChange={this.selectOption.bind(this)} /> Medium (25-50 Songs)
-                      </label>
-                      <label className="radio-inline">
-                        <input type="radio" name="sizeOptions" value="l" onChange={this.selectOption.bind(this)} /> Large (50+ Songs)
-                      </label>
-                    </div>
-                  </form>
+              <div className="modal-body">
+                <div className="name">
+                  <input className="text name" className="form-control" placeholder="Playlist Name" onChange={this.selectOption.bind(this)}/>
                 </div>
-                <div className="modal-footer">
-                  <div className="btn new-playlist-button text-center"  data-dismiss="modal" onClick={this.createPlaylist.bind(this)}>
-                    CONTINUE
+              <div class="row dropdown-row">
+                <div class="col-xs-6 text-center">
+                  <div class="dropdown-label">Type:</div>
+                  <select name="typeOptions" onChange={this.selectOption.bind(this)}>
+                    {typeOptions.map(opt => (
+                          <option key={opt.value} value={opt.value}>{opt.label}</option>
+                      ))}
+                  </select>
+                </div>
+                <div class="col-xs-6 text-center">
+                  <div class="dropdown-label">Size:</div>
+                  <select onChange={this.selectOption.bind(this)}>
+                    <option name="sizeOptions" value="s">Small (25 songs)</option>
+                    <option name="sizeOptions" value="m">Medium (50 songs)</option>
+                    <option name="sizeOptions" value="l">Large (100 songs)</option>
+                  </select>
+                </div>
+              </div>
+              <hr/>
+              <div class="media">
+                <div class="media-left">
+                  <div class="media-object">
+                    <div className="btn new-playlist-button text-center"  data-dismiss="modal" onClick={this.createPlaylist.bind(this)}>
+                      CONTINUE
+                    </div>
+                  </div>
+                </div>
+                <div class="media-body media-middle">
+                  <div class="">
+                    Next: invite friends
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
+        </div>
+      </div>
         );
     }
 }
 
+
+
+
+
 export default CreatePlaylistModal;
+
